bd696c17fef1abdb2454569053ccf1ac1bd9b45b
LaurelButler <laurelbutler126@gmail.com>
2019-06-25 13:54:40 -0400

    set up function stubs

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+/* global $ */
+
+const STORE = {
+    mealDetails: [],
+    tips: [],
+};
+
+function generateCustomerChargesHTML() {
+
+}
+
+function generateEarningsInfoHTML() {
+
+}
+
+function renderStore() {
+
+}
+
+function addMealDetails() {
+
+}
+
+function addTips() {
+
+}
+
+function clearMealDetails() {
+
+}
+
+function clearTips() {
+
+}
+
+function handleMealDetailsSubmit() {
+
+}
+
+function handleResetButton() {
+
+}
+
+function main() {
+    
+}
